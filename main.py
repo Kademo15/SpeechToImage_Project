eb6bb9a59685d1bd80988c7c604c98c3686b652d
Kademo15 <kangee.2004@gmail.com>
2024-04-25 13:56:07 +0200

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 import json
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
+import socket
 
 
 #Setup flask and socketio
@@ -29,16 +30,123 @@ def home():
     return render_template('index.html')
 
 
-@socketio.on('message')
+@socketio.on('/message')
 def handle_message(data):
+    print("Message received")
     history.append({"role": "user", "content": data['text']})
     completion = client.chat.completions.create(
         model="local-model",  # this field is currently unused
         messages=history,
         temperature=0.7,
     )
-    history.append({"role": "assistant", "content": completion.choices[0].message['content']})
-    emit('answer', {'text': completion.choices[0].message['content']})
+    print("Ai response: ", completion.choices[0].message.content)
+    history.append({"role": "assistant", "content": completion.choices[0].message.content})
+    emit('answer', {'text': completion.choices[0].message.content})
+    preparePromptAndRequest(completion.choices[0].message.content)
+
+def requestComfyUi(llmPrompt):
+    p = {"prompt": llmPrompt}
+    data = json.dumps(p).encode('utf-8')
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)
+
+def preparePromptAndRequest(prompt):
+    
+    prompt_text = """
+{
+    "3": {
+        "class_type": "KSampler",
+        "inputs": {
+            "cfg": 8,
+            "denoise": 1,
+            "latent_image": [
+                "5",
+                0
+            ],
+            "model": [
+                "4",
+                0
+            ],
+            "negative": [
+                "7",
+                0
+            ],
+            "positive": [
+                "6",
+                0
+            ],
+            "sampler_name": "euler",
+            "scheduler": "normal",
+            "seed": 8566257,
+            "steps": 20
+        }
+    },
+    "4": {
+        "class_type": "CheckpointLoaderSimple",
+        "inputs": {
+            "ckpt_name": "v1-5-pruned-emaonly.ckpt"
+        }
+    },
+    "5": {
+        "class_type": "EmptyLatentImage",
+        "inputs": {
+            "batch_size": 1,
+            "height": 512,
+            "width": 512
+        }
+    },
+    "6": {
+        "class_type": "CLIPTextEncode",
+        "inputs": {
+            "clip": [
+                "4",
+                1
+            ],
+            "text": "masterpiece best quality girl"
+        }
+    },
+    "7": {
+        "class_type": "CLIPTextEncode",
+        "inputs": {
+            "clip": [
+                "4",
+                1
+            ],
+            "text": "bad hands"
+        }
+    },
+    "8": {
+        "class_type": "VAEDecode",
+        "inputs": {
+            "samples": [
+                "3",
+                0
+            ],
+            "vae": [
+                "4",
+                2
+            ]
+        }
+    },
+    "9": {
+        "class_type": "SaveImage",
+        "inputs": {
+            "filename_prefix": "ComfyUI",
+            "images": [
+                "8",
+                0
+            ]
+        }
+    }
+}
+"""
+
+    prompt = json.loads(prompt_text)
+    #set the text prompt for our positive CLIPTextEncode
+    prompt["6"]["inputs"]["text"] = prompt
+
+
+    requestComfyUi(prompt)
 
 if __name__ == '__main__':
     # Use app.run() instead of socketio.run() when running in a Jupyter notebook
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 var socket = io.connect('http://localhost:5000');
 var recognition = new webkitSpeechRecognition();
+var transcript;
 //recognition.continuous = false;
 //recognition.interimResults = false;
 recognition.lang = 'en-US';
@@ -9,16 +10,18 @@ document.getElementById('start-microphone-button').addEventListener('click', fun
 });
 
 recognition.onresult = function(event) {
-    var transcript = event.results[event.results.length - 1][0].transcript;
+    transcript = event.results[event.results.length - 1][0].transcript;
     document.getElementById('transcription-textbox').value = transcript;
-    socket.emit('message', { 'text': transcript });
+    
 };
 
 recognition.onspeechend = function () {
-    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+    //action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+    socket.emit('/message', { 'text': transcript });
     recognition.stop();
 }
 
 socket.on('answer', function(data) {
     document.getElementById('answer-textbox').value = data.text;
+    
 });
